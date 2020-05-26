ab14bdb581043af0dc1020bb12e2b1ca32008437
ASUS <ASUS@LAPTOP-LS3ETHPQ>
2020-05-26 09:30:08 +0700

    xxsss

--- QLTT/Forms/SanPhamForm.Designer.cs
+++ QLTT/Forms/SanPhamForm.Designer.cs
@@ -48,12 +48,12 @@
             this.btnSave.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnSave.FlatAppearance.BorderSize = 0;
             this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSave.ForeColor = System.Drawing.Color.White;
-            this.btnSave.Location = new System.Drawing.Point(34, 296);
+            this.btnSave.Location = new System.Drawing.Point(67, 348);
             this.btnSave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(148, 52);
+            this.btnSave.Size = new System.Drawing.Size(123, 39);
             this.btnSave.TabIndex = 29;
             this.btnSave.Text = "Lưu";
             this.btnSave.UseVisualStyleBackColor = false;
@@ -65,12 +65,12 @@
             this.btnReset.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnReset.FlatAppearance.BorderSize = 0;
             this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnReset.ForeColor = System.Drawing.Color.White;
-            this.btnReset.Location = new System.Drawing.Point(347, 296);
+            this.btnReset.Location = new System.Drawing.Point(315, 348);
             this.btnReset.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnReset.Name = "btnReset";
-            this.btnReset.Size = new System.Drawing.Size(148, 52);
+            this.btnReset.Size = new System.Drawing.Size(136, 39);
             this.btnReset.TabIndex = 27;
             this.btnReset.Text = "Hủy";
             this.btnReset.UseVisualStyleBackColor = false;
@@ -80,11 +80,11 @@
             // 
             this.txtGiaban.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.txtGiaban.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtGiaban.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtGiaban.Location = new System.Drawing.Point(188, 230);
+            this.txtGiaban.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGiaban.Location = new System.Drawing.Point(188, 266);
             this.txtGiaban.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtGiaban.Name = "txtGiaban";
-            this.txtGiaban.Size = new System.Drawing.Size(297, 20);
+            this.txtGiaban.Size = new System.Drawing.Size(297, 22);
             this.txtGiaban.TabIndex = 3;
             this.txtGiaban.TabStop = false;
             this.txtGiaban.Validating += new System.ComponentModel.CancelEventHandler(this.txtGiaban_Validating);
@@ -93,12 +93,12 @@
             // 
             this.txtCongdung.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.txtCongdung.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtCongdung.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCongdung.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtCongdung.Location = new System.Drawing.Point(188, 157);
             this.txtCongdung.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtCongdung.Multiline = true;
             this.txtCongdung.Name = "txtCongdung";
-            this.txtCongdung.Size = new System.Drawing.Size(297, 54);
+            this.txtCongdung.Size = new System.Drawing.Size(297, 94);
             this.txtCongdung.TabIndex = 1;
             this.txtCongdung.TabStop = false;
             this.txtCongdung.Validating += new System.ComponentModel.CancelEventHandler(this.txtCongdung_Validating);
@@ -107,11 +107,11 @@
             // 
             this.txtTen.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.txtTen.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTen.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtTen.Location = new System.Drawing.Point(186, 117);
             this.txtTen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtTen.Name = "txtTen";
-            this.txtTen.Size = new System.Drawing.Size(297, 20);
+            this.txtTen.Size = new System.Drawing.Size(297, 22);
             this.txtTen.TabIndex = 1;
             this.txtTen.TabStop = false;
             this.txtTen.Validating += new System.ComponentModel.CancelEventHandler(this.txtTen_Validating);
@@ -119,40 +119,40 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.label8.Location = new System.Drawing.Point(30, 231);
+            this.label8.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.SeaGreen;
+            this.label8.Location = new System.Drawing.Point(30, 265);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(76, 19);
+            this.label8.Size = new System.Drawing.Size(78, 23);
             this.label8.TabIndex = 14;
             this.label8.Text = "Giá bán*";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label7.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.SeaGreen;
             this.label7.Location = new System.Drawing.Point(30, 158);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(103, 19);
+            this.label7.Size = new System.Drawing.Size(101, 23);
             this.label7.TabIndex = 13;
             this.label7.Text = "Công dụng*";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label2.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.SeaGreen;
             this.label2.Location = new System.Drawing.Point(30, 117);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 19);
+            this.label2.Size = new System.Drawing.Size(46, 23);
             this.label2.TabIndex = 22;
             this.label2.Text = "Tên*";
             // 
             // label1
             // 
-            this.label1.Font = new System.Drawing.Font("Arial", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Font = new System.Drawing.Font("Calibri", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SeaGreen;
             this.label1.Location = new System.Drawing.Point(3, 21);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(548, 50);
@@ -169,7 +169,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
-            this.ClientSize = new System.Drawing.Size(548, 384);
+            this.ClientSize = new System.Drawing.Size(548, 425);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.txtGiaban);
